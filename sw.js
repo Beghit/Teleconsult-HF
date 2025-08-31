9da878663a741e059d355f486c1b0cc26afd451b
Beghit <Belghait.elhajjaj@gmail.com>
2025-08-31 13:17:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,10 @@ const CACHE_NAME = 'heartguardian-v3';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './192-icon.png',
+  './512-icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 self.addEventListener('install', (event) => {
@@ -41,7 +44,7 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', (event) {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
